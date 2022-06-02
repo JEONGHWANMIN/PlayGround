3ae8179e4fd260d104d4fba9fa7c92e05317d088
JEONGHWANMIN <mimi0j0j@naver.com>
2022-06-03 02:22:01 +0900

    PlayGround ~

--- Study.js
+++
@@ -1,8 +0,0 @@
-function Person(name) {
-  this.name = name;
-}
-
-const me = new Person('Hwan');
-
-console.log(Person.prototype === me.__proto__);
-console.log(me.constructor === Person);
--- commit.sh
+++ commit.sh
@@ -4,7 +4,7 @@ echo '##### auto push start #####'
 cd ~/ml/PlayGround
 git add .
 git status
-git commit -m "PlayGround ~ 🌟"
+git commit -m "PlayGround ~ "
 git push play master
 
 echo '##### auto push end #####'
