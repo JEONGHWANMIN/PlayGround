39496dd4a85bed011ff14a48128b4429611f6f03
JEONGHWANMIN <mimi0j0j@naver.com>
2023-01-07 23:51:12 +0900

    for-of 프로토콜 이터레이터 연산

--- test.js
+++ test.js
@@ -2,6 +2,10 @@ const arr = [1, 2, 3, 4, 5];
 
 const iter = arr[Symbol.iterator]();
 
+for (let i of iter) {
+  console.log(i);
+}
+
 console.log(iter.next());
 console.log(iter.next());
 console.log(iter.next());
