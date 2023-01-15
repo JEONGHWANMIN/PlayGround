31c0c1b4cee1f5281a12f697d35b0367a3f1fa84
JEONGHWANMIN <mimi0j0j@naver.com>
2023-01-15 23:58:51 +0900

    Feat : 리듀스 예시

--- 함수형프로그래밍-ES6+/reduce.js
+++ 함수형프로그래밍-ES6+/reduce.js
@@ -13,3 +13,18 @@ const reduce = (f, acc, iter) => {
 
 console.log(reduce(add, 0, [1, 2, 3, 4, 5]));
 console.log(reduce(add, [1, 2, 3, 4, 5]));
+
+const products = [
+  { name: "반팔티", price: 15000 },
+  { name: "긴팔티", price: 20000 },
+  { name: "반바지", price: 15000 },
+  { name: "핸드폰케이스", price: 25000 },
+  { name: "후드티", price: 40000 },
+  { name: "긴바지", price: 30000 },
+];
+
+const addPrice = (acc, obj) => {
+  return acc + obj.price;
+};
+
+console.log(reduce(addPrice, 0, products));
