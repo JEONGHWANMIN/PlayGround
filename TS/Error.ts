f0bdfbb72ef208d25aa8842a142fcccff57b95bc
JEONGHWANMIN <mimi0j0j@naver.com>
2023-01-23 23:55:38 +0900

    Error 타입 설정

---
+++ TS/Error.ts
@@ -0,0 +1,15 @@
+const reportError = ({ message }) => {
+  console.log(message);
+};
+
+try {
+  throw new Error("this is try block");
+} catch (error) {
+  let message;
+  if (error instanceof Error) message = error.message;
+  else message = String(error);
+  // we'll proceed, but let's report it
+  reportError({ message });
+}     
+
+export {};
