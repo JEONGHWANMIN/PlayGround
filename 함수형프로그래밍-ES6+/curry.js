32dfd91d91bea7a2c8d61afd0dd4fff889eaf5c2
JEONGHWANMIN <mimi0j0j@naver.com>
2023-05-08 00:07:11 +0900

    curry update

--- 함수형프로그래밍-ES6+/curry.js
+++ 함수형프로그래밍-ES6+/curry.js
@@ -1,12 +1,17 @@
-const curry =
-  (f) =>
-  (a, ..._) => {
-    return _.length ? f(a) : (..._) => f(a, ..._);
+const curry = (f) => {
+  return (a, ...args) => {
+    if (args.length) {
+      return f(a, ...args);
+    } else {
+      return (...rest) => {
+        console.log("Rest", rest);
+        return f(a, ...rest);
+      };
+    }
   };
+};
 
 const mult = curry((a, b) => a * b);
 
-console.log(mult(2, 5));
-console.log(mult(2));
-console.log(mult(2)(5));
-console.log(mult(2)(5));
+console.log(mult(2)(8));
+console.log(mult(2, 10));
