26819d8103522c35f512e2dc3dab9cefea8581ea
JEONGHWANMIN <ghksals09041@gmail.com>
2023-05-20 01:07:33 +0900

    Feat : 프로로타입 패턴

--- 디자인패턴/생성패턴/prototype.js
+++ 디자인패턴/생성패턴/prototype.js
@@ -1,21 +1,68 @@
-class Dog {
+function Dog(name) {
+  this.name = name;
+
+  this.bark = () => {
+    return `Woof!`;
+  };
+}
+
+const dog1 = new Dog("환고");
+const dog2 = new Dog("환고");
+const dog3 = new Dog("환고");
+
+console.log(dog1.bark());
+
+function Cat(name) {
+  this.name = name;
+}
+
+Cat.prototype.bark = () => `mewmew!`;
+
+const cat1 = new Cat("얀고");
+const cat2 = new Cat("얀고");
+const cat3 = new Cat("얀고");
+
+console.log(cat1.bark());
+
+class Panda {
   constructor(name) {
     this.name = name;
   }
 
   bark() {
-    return `Woof!`;
+    return "papap";
+  }
+
+  bark2() {
+    return "papap";
   }
 }
 
-const dog1 = new Dog("Daisy");
-const dog2 = new Dog("Max");
-const dog3 = new Dog("Spot");
+class SubPanda extends Panda {
+  constructor(name) {
+    super(name);
+  }
 
-console.log(dog1.bark());
-console.log(dog2.bark());
-console.log(dog3.bark());
+  bark() {
+    return "papap2";
+  }
+}
 
-Dog.prototype.play = () => console.log("플레이 ~~");
+const panda1 = new Panda("판다1"); //__proto__
+const panda2 = new Panda("판다1"); //__proto__
+const panda3 = new Panda("판다1"); //__proto__
 
-dog1.play();
+const subPanda1 = new SubPanda("서브판다"); // __proto__
+
+console.log(subPanda1.bark2());
+
+// console.log(panda1.bark());
+
+// console.log(Panda.prototype);
+// console.log(panda1.__proto__);
+
+const a = [];
+
+Array.prototype.hwanmin = () => console.log("먕먕");
+
+a.hwanmin();
