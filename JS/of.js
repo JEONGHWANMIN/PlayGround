1e5f76ef0be10cc1daba2241351a2afc37e036db
JEONGHWANMIN <mimi0j0j@naver.com>
2023-07-23 22:42:22 +0900

    Feat : Array.of

---
+++ JS/of.js
@@ -0,0 +1,3 @@
+const arr = Array.of(1, 2, 3, 4, 5);
+
+console.log(arr);
--- JS/values.js
+++ JS/values.js
@@ -1,6 +1,7 @@
 const array1 = ["a", "b", "c"];
 const iterator = array1.values();
 
+const iter = array1[Symbol.iterator]();
 console.log(iterator);
 
 for (const value of iterator) {
